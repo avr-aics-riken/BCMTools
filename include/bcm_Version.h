ab410eaa8ae131895498416e02ecc8b79b418d52
Kenji Ono <keno@riken.jp>
2014-01-25 17:34:01 +0900

    r10 move FileIO to HDMlib [ver 0.9.3]

--- include/bcm_Version.h
+++ include/bcm_Version.h
@@ -1,10 +1,10 @@
 /*
  * BCMTools
  *
- * Copyright (C) 2011-2013 Institute of Industrial Science, The University of Tokyo.
+ * Copyright (C) 2011-2014 Institute of Industrial Science, The University of Tokyo.
  * All rights reserved.
  *
- * Copyright (c) 2012-2013 Advanced Institute for Computational Science, RIKEN.
+ * Copyright (c) 2012-2014 Advanced Institute for Computational Science, RIKEN.
  * All rights reserved.
  *
  */
@@ -18,10 +18,10 @@
 #define _BCM_VERSION_H_
 
 /** BCMToolsのバージョン */
-#define BCM_VERSION_NO  "0.9.2"
+#define BCM_VERSION_NO  "0.9.3"
 
 /** BCMToolsのリビジョン */
-#define BCM_REVISION    "20131216_1700"
+#define BCM_REVISION    "20140125_1730"
 
 #endif /* _BCM_VERSION_H_ */
 
